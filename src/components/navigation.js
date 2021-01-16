0b3aabfcb821e240ec799bae21ce5d08b1084e9d
MarkoIvanetic <manna.grad@gmail.com>
2021-01-16 04:36:58 +0100

    add page

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby-plugin-intl';
+import { FormattedMessage, Link } from 'gatsby-plugin-intl';
 import styles from './style/navigation.module.scss';
 
 const ListLink = (props) => (
@@ -10,10 +10,20 @@ const ListLink = (props) => (
 
 export const Navigation = () => (
   <ul role="navigation" className={styles.list}>
-    <ListLink to="/">Home</ListLink>
-    <ListLink to="/photos/">Photos</ListLink>
-    <ListLink to="/about/">About</ListLink>
-    <ListLink to="/blog/">Blog</ListLink>
-    <ListLink to="/contact/">Contact</ListLink>
+    <ListLink to="/">
+      <FormattedMessage id="nav_0" />
+    </ListLink>
+    <ListLink to="/ovcar/">
+      <FormattedMessage id="nav_1" />
+    </ListLink>
+    <ListLink to="/photos/">
+      <FormattedMessage id="nav_2" />
+    </ListLink>
+    <ListLink to="/blog/">
+      <FormattedMessage id="nav_3" />
+    </ListLink>
+    <ListLink to="/contact/">
+      <FormattedMessage id="nav_4" />
+    </ListLink>
   </ul>
 );
---
+++ src/pages/ovcar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+// import { Link, graphql } from "gatsby"
+import styles from '../styles/index.scss';
+import Layout from '../components/layout';
+
+import { PlaceholderImage } from '../components/placeholderImage';
+import { FormattedMessage } from 'gatsby-plugin-intl';
+
+const About = ({ data }) => {
+  return (
+    <Layout>
+      <div>
+        <PlaceholderImage width="100%" />
+      </div>
+
+      <div>
+        <h5>
+          <FormattedMessage id="ovcar_h5" />
+        </h5>
+        <p>
+          <FormattedMessage id="ovcar_p1" />
+        </p>
+        <p>
+          <FormattedMessage id="ovcar_p2" />
+        </p>
+        <p>
+          <FormattedMessage id="ovcar_p3" />
+        </p>
+        <p>
+          <FormattedMessage id="ovcar_p4" />
+        </p>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
+
+// export const query = graphql`
+//   query BlogPostsPageQuery {
+//     allContentfulBlogPost(limit: 1000) {
+//       totalCount
+//       edges {
+//         node {
+//           id
+//           title
+//           slug
+//         }
+//       }
+//     }
+//   }
+// `
