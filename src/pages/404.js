9c7364991655d3e87d04406e3c01e9a53aa5c718
Marko Ivanetic <marko.ivanetic@burza.hr>
2021-01-26 19:33:20 +0100

    feat(404): add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import { FormattedMessage, injectIntl } from 'gatsby-plugin-intl';
-
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'gatsby-plugin-intl';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import Layout from '../components/layout';
-import SEO from '../components/seo';
 
-const NotFoundPage = ({ intl }) => (
-  <Layout>
-    <SEO lang={intl.locale} title={`404: ${intl.formatMessage({ id: 'metadata_title' })}`} />
-    <h1>
-      <FormattedMessage id="metadata_subtitle" />
-    </h1>
-    <p>
-      <FormattedMessage id="metadata_subtitle" />
-    </p>
-  </Layout>
-);
+const NotFoundPage = ({ data }) => {
+  const image = data.contentfulAsset;
+  return (
+    <Layout>
+      <Img fluid={image.fluid} alt={image.title} />
+      <h1>
+        <FormattedMessage id="404_title" />
+      </h1>
+      <p>
+        <FormattedMessage id="404_subtitle" />
+      </p>
+    </Layout>
+  );
+};
 
-export default injectIntl(NotFoundPage);
+export const query = graphql`
+  {
+    contentfulAsset(file: { fileName: { eq: "404.png" } }) {
+      title
+      fluid(maxWidth: 700) {
+        base64
+        aspectRatio
+        src
+        srcSet
+        sizes
+      }
+    }
+  }
+`;
+
+NotFoundPage.propTypes = {
+  data: PropTypes.shape({
+    contentfulAsset: PropTypes.object,
+  }),
+};
+
+export default NotFoundPage;
